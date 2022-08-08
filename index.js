a130ec9d5c898b57cb222e9fcd6df97b72f7c57c
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-08 23:15:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+function appendCat(name) {
+  return [...cats, name];
+}
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeLastCat() {
+  return [...cats.slice(0, -1)];
+}
+function removeFirstCat(){
+    return [...cats.slice(1)]
+}
